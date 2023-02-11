8ac1d2181cd037d5ea6a99d774715aef70cadbd0
marinakravchuk <105381706+marinakravchuk@users.noreply.github.com>
2023-02-11 19:52:11 +0200

    update
    footer
    comments
    submit

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    nav: true,
+    autoplay: false,
+    autoplayTimeout: false,
+
+  });
+});
