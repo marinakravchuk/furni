b05b2bc2fcfeeafafd0780b39e7b3afce94cfe9a
marinakravchuk <105381706+marinakravchuk@users.noreply.github.com>
2023-02-25 23:25:57 +0200

    test modal

--- main.js
+++ main.js
@@ -11,3 +11,31 @@ $(function() {
 
   });
 });
+
+
+const openButton = document.querySelector("[data-modal-button]");
+const modal = document.querySelector("[data-modal]");
+const closeButton = document.querySelector("[data-modal-close]");
+const openBurgerButton = document.querySelector(".burger_btn");
+const menuHhamburger = document.querySelector(".hidden_burger");
+
+
+openButton.addEventListener("click", ()=>{
+  modal.classList.remove("hidden")
+})
+
+modal.addEventListener("click", ()=> {
+  modal.classList.add("hidden")
+})
+
+modal.querySelector(".modal_window").addEventListener("click", function(event){
+event.stopPropagation();
+})
+
+closeButton.addEventListener("click", ()=>{
+  modal.classList.add("hidden")
+})
+
+openBurgerButton.addEventListener("click", ()=>{
+  menuHhamburger.classList.toggle("hidden_burger")
+})
